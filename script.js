a64cec0e6b86de14b83655d32c3706d4140eb0b7
jtithy <tithyjebunnesa202@gmail.com>
2026-07-31 14:35:02 +0600

    Bomb Class

--- script.js
+++ script.js
@@ -88,6 +88,32 @@ class Fruit {
     }
 }
 
+//Bomb Class
+class Bomb {
+    constructor() {
+        this.radius = 25;
+        this.x = Math.random() * (canvas.width - 100) + 50;
+        this.y = canvas.height + 50;
+        this.vx = (Math.random() - .5) * 8;
+        this.vy = -(Math.random() * 6 + 12);
+    }
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+
+        this.vy += gravity;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.strokeStyle = "red";
+        ctx.lineWidth = 3;
+        ctx.stroke();
+    }
+}
+
 //Spawn Fruit
 function spawnFruit() {
     fruits.push(new Fruit());
